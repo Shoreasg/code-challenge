ffc2977883720a919faf9dff870449c59bf01e82
Wei Lun <garyganweilun@gmail.com>
2022-05-22 06:20:52 +0800

    Fix address

--- src/problem4/retrieve-holders.ts
+++ src/problem4/retrieve-holders.ts
@@ -3,7 +3,7 @@ const web3 = new Web3("https://bsc-dataseed.binance.org/") //provider from BSC
 
 const tokenContract: string = "0x250b211ee44459dad5cd3bca803dd6a7ecb5d46c" //OLD SWTH contract
 
-const addresses: string[] = ["1", "0x0020c5222a24e4a96b720c06b803fb8d34adc0af", "0xfe808b079187cc460f47374580f5fb47c82b87a5"] //List of address to look for
+const addresses: string[] = ["0x123d475e13aa54a43a7421d94caa4459da021c77", "0x0020c5222a24e4a96b720c06b803fb8d34adc0af", "0xfe808b079187cc460f47374580f5fb47c82b87a5"] //List of address to look for
 
 const abi: any = [{ //ABI interface of contract
     "inputs": [
