3c8176a074e0ed7f41cd21d958e3b09312e27b1d
Wei Lun <garyganweilun@gmail.com>
2022-05-21 16:14:02 +0800

    Added comments for promblem 1

--- src/problem1/index.js
+++ src/problem1/index.js
@@ -7,7 +7,7 @@
 //Output: return - summation to n, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
 
-var sum_to_n_a = function (n) {
+var sum_to_n_a = function (n) { // O(N)
     let sum = 0 // let sum = 0
     for (let i = 1; i <= n; i++) { //iterate from 1 to N
         sum += i; // Each iteration, i is added to sum, then i is increase by 1. //When i becomes greater than N, test condition is false              
@@ -17,7 +17,7 @@ var sum_to_n_a = function (n) {
 
 console.log(sum_to_n_a(5))
 
-var sum_to_n_b = function (n) {
+var sum_to_n_b = function (n) { // O(N)
     if (n > 0) //if number is 5, i will return 5 + sum_to_n_b(4), call again until the number reaches 0
         return n + sum_to_n_b(n - 1);
     return 0 //else if user enter a negative number, i will return 0
@@ -25,8 +25,8 @@ var sum_to_n_b = function (n) {
 
 console.log(sum_to_n_b(5))
 
-var sum_to_n_c = function (n) {
-    return n * (n + 1) / 2;
+var sum_to_n_c = function (n) { //O(N^2)
+    return n * (n + 1) / 2; // using Gauss sumation formula
 };
 
 console.log(sum_to_n_c(5))
